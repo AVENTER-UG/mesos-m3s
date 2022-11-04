16d131aaf77ad2497d6e473e1a3df290143829d7
Andreas Peters <ap@aventer.biz>
2022-11-04 13:03:58 +0100

    ADD: ssl config.cnf

--- docs/examples/generate-certs-2.sh
+++ docs/examples/generate-certs-2.sh
@@ -1,16 +1,44 @@
 #! /bin/bash
 
-DOMAIN="localhost.local"
-
+NAME="m3sdatastore"
+DOMAIN="mini"
 HOSTS="server client"
 
-SUBJ_BASE="/C=DE/ST=SH/L=E/O=MariaDB/OU=Support/emailAddress=nothing@example.com"
+cat > config.cnf <<EOF
+[ req ]
+prompt = no
+default_bits = 4096
+default_md = sha256
+distinguished_name = req_distinguished_name
+x509_extensions = v3_ca
+req_extensions = v3_req
 
-CA_SUBJ="$SUBJ_BASE/CN=$DOMAIN"
+[ v3_ca ]
+subjectKeyIdentifier=hash
+authorityKeyIdentifier=keyid:always,issuer
+basicConstraints = CA:true
+
+[req_distinguished_name]
+countryName = DE
+stateOrProvinceName = SH
+localityName = EN
+organizationalUnitName	= IT
+
+[ v3_req ]
+# Extensions to add to a certificate request
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ${NAME}
+DNS.2 = *.mini
+DNS.3 = ${NAME}.${DOMAIN}
+EOF
 
 openssl genrsa 2048 > ca-key.pem
 
-openssl req -sha256 -new -x509 -nodes -days 3600 -key ca-key.pem -out ca-cert.pem -subj $CA_SUBJ
+openssl req -sha256 -new -x509 -nodes -days 3600 -key ca-key.pem -out ca-cert.pem -subj '/CN=${NAME}.${DOMAIN}' -config ./config.cnf
 
 
 for HOST in $HOSTS
@@ -19,7 +47,7 @@ do
 
 	# Create server certificate, remove passphrase, and sign it
 	# Create the server key
-  openssl req -sha256 -newkey rsa:2048 -days 3600 -nodes -keyout $HOST-key.pem -out $HOST-req.pem -subj $SUBJ
+  openssl req -sha256 -newkey rsa:2048 -days 3600 -nodes -keyout $HOST-key.pem -out $HOST-req.pem -config <( cat config.cnf )
 
 	# Process the server RSA key
 	openssl rsa -in $HOST-key.pem -out $HOST-key.pem
@@ -30,5 +58,3 @@ do
 	# Verify certificates
 	openssl verify -CAfile ca-cert.pem $HOST-cert.pem
 done
-
-
